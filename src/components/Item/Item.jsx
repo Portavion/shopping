e4f968e8371a9fc88e1139de3c999f0528dcd9b4
Portavion <156545430+Portavion@users.noreply.github.com>
2024-12-11 17:08:37 +0000

    feat: fetching items w/ API and rendering item cars

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,26 +1,17 @@
-import React from "react";
 import styles from "./Item.module.css";
 
 const Item = ({
   itemName = "Test Item Name",
   itemImgSrc = "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR59Yy81pOzQg9iRFnvtCukGJDcb1peb_3iI1QjFtc5mKWxlI58ZQt2qP36d6P9mMtv-P0R5iFdRSvXXdvZG5U2gfNQGRArXh8DcNGjpKerOMGynuG1BBnzwcp6",
-  itemPrice = 134.48,
-  itemRating = 4,
-  itemRatingCount = 12,
+  itemPrice = 0,
 }) => {
   return (
     <div className={styles.itemCard}>
       <img src={itemImgSrc} alt={itemName} />
       <h3>{itemName}</h3>
       <h2>{itemPrice}$</h2>
-      <div className={styles.rating}>
-        <div>{itemRating} stars</div>
-        <div> ({itemRatingCount})</div>
-      </div>
-      <div className={styles.cartButtonContainer}>
-        <button>Quick Add</button>
-        <button>Add to cart</button>
-      </div>
+      <input type="number" min="0" name="quantity" placeholder="0" />
+      <button>Add to cart</button>
     </div>
   );
 };
--- src/components/ShoppingPage/ShoppingPage.jsx
+++ src/components/ShoppingPage/ShoppingPage.jsx
@@ -1,15 +1,41 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import { Item } from "../Item/Item";
 import styles from "./ShoppingPage.module.css";
 
 const Shop = () => {
-  return (
-    <div className={styles.itemsContainer}>
-      <Item />
-      <Item />
-      <Item />
-    </div>
-  );
+  const [items, setItems] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=6", { mode: "cors" })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => setItems(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <p>Loading</p>;
+  } else if (error) {
+    return <p>An error has occured: {error}</p>;
+  }
+  console.log(items);
+  const itemCards = items.map((item) => {
+    return (
+      <Item
+        id={item.id}
+        itemName={item.title}
+        itemImgSrc={item.image}
+        itemPrice={item.prive}
+      ></Item>
+    );
+  });
+  console.log({ itemCards });
+  return <div className={styles.itemsContainer}>{itemCards}</div>;
 };
 
 export { Shop };
